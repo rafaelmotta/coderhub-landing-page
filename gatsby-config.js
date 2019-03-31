c96eba6b7110eda0d90176a841a641954bd92a28
Rafael Michels Motta <rafael.motta@personio.de>
2019-03-31 20:37:35 +0200

    Fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `CoderHub`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
