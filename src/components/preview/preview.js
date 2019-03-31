d1306cd49bd0c0cc5424bad8a18d195fecffa16b
Rafael Michels Motta <rafael.motta@personio.de>
2019-03-31 20:48:26 +0200

    Preview img

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -6,24 +6,22 @@ import s from "./preview.module.scss"
 
 const Preview = () => (
   <section className={s.previewContainer}>
-    <div className="container">
-      <StaticQuery
-        query={graphql`
-          query {
-            placeholderImage: file(relativePath: { eq: "codehub.png" }) {
-              childImageSharp {
-                fluid(maxWidth: 960) {
-                  ...GatsbyImageSharpFluid
-                }
+    <StaticQuery
+      query={graphql`
+        query {
+          placeholderImage: file(relativePath: { eq: "codehub.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 960) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
-        `}
-        render={data => (
-          <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-        )}
-      />
-    </div>
+        }
+      `}
+      render={data => (
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+      )}
+    />
   </section>
 )
 
